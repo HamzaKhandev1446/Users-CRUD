85fec8c722a5cb0fc95fe1fcfd21a5c9eea73771
Hamza Khan <hamzakhan.dev1446@gmail.com>
2023-05-26 20:53:53 +0500

    Edit and delete feature added to users.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { USERS } from './models/users.model';
 import { User } from 'src/types/User';
 
@@ -7,7 +7,7 @@ import { User } from 'src/types/User';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Users-CRUD';
   jobModes: string[] = ['Onsite', 'Remote', 'All'];
   jobTypes: string[] = ['Full-time', 'Part-time', 'Contractual'];
@@ -24,31 +24,86 @@ export class AppComponent {
   usersList: User[] = USERS;
 
   ngOnInit() {
+    // Initialize the filteredList with the usersList
     this.filteredList = this.usersList;
+
+    // Extract the list of all available designations from the usersList
     this.designations = this.extractValidData(this.usersList);
   }
 
+  /**
+   * Event handler for filter selection.
+   * @param filter The selected filter value
+   * @param type The type of filter (jobMode or jobType)
+   */
   onFilterSelected(filter: string, type: string) {
+    // Update the filteredJobMode or filteredJobType based on the type
     type === 'jobMode'
       ? (this.filteredJobMode = filter)
       : (this.filteredJobType = filter);
+
+    // Apply filters to the usersList
     this.filteredList = this.usersList
       .filter((user: User) => {
-        if (this.filteredJobMode === 'All' || this.filteredJobMode === '')
+        // Filter by jobMode
+        if (this.filteredJobMode === 'All' || this.filteredJobMode === '') {
           return user;
-        else {
+        } else {
           return user.jobMode === this.filteredJobMode;
         }
       })
       .filter((user: User) => {
-        if (this.filteredJobType === '') return user;
-        else {
+        // Filter by jobType
+        if (this.filteredJobType === '') {
+          return user;
+        } else {
           return user.jobType === this.filteredJobType;
         }
       });
   }
 
-  extractValidData(usersList: User[]) {
+  /**
+   * Update the user in the usersList.
+   * @param updatedUser The updated user object
+   */
+  submitEditUser(updatedUser: User) {
+    // Find the index of the user to be updated
+    const userId = this.findUserId(this.usersList, updatedUser);
+
+    // Replace the user at the found index with the updated user
+    this.usersList.splice(userId, 1, updatedUser);
+  }
+
+  /**
+   * Delete the user from the usersList.
+   * @param deletedUser The user object to be deleted
+   */
+  submitDeleteUser(deletedUser: User) {
+    // Find the index of the user to be deleted
+    const userId = this.findUserId(this.usersList, deletedUser);
+
+    // Remove the user from the usersList
+    this.usersList.splice(userId, 1);
+  }
+
+  /**
+   * Extracts the list of all available designations from the given users list.
+   * @param usersList The list of users
+   * @returns An array containing unique designations
+   */
+  extractValidData(usersList: User[]): string[] {
     return Array.from(new Set(usersList.map((user) => user.designation)));
+  }
+
+  /**
+   * Find the index of the user in the given users list.
+   * @param users The list of users
+   * @param updatedUser The user object to be found
+   * @returns The index of the user in the users list
+   */
+  findUserId(users: User[], updatedUser: User): number {
+    return users.findIndex((findUser: User) => {
+      return findUser.id === updatedUser.id;
+    });
   }
 }
